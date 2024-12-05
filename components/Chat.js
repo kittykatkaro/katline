d672613161c0e8f361c2b8f192fad32cda3e06b5
Karo <karoogorek@gmail.com>
2024-12-05 09:59:51 +0100

    Implement chat functionality with GiftedChat and initial message setup

--- components/Chat.js
+++ components/Chat.js
@@ -1,17 +1,55 @@
-import { useEffect } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { useEffect, useState } from 'react';
+import {
+	StyleSheet,
+	View,
+	Text,
+	Platform,
+	KeyboardAvoidingView,
+} from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat';
 
 const Chat = ({ route, navigation }) => {
 	const { name, color } = route.params;
+	const [messages, setMessages] = useState([]);
+	// Function to send messages
+	const onSend = (newMessages = []) => {
+		setMessages((previousMessages) =>
+			GiftedChat.append(previousMessages, newMessages)
+		);
+	};
 
 	useEffect(() => {
+		// Set the initial message
+		setMessages([
+			{
+				_id: 1,
+				text: 'Hello! Start Chatting!',
+				createdAt: new Date(),
+				user: {
+					_id: 2,
+					name: 'React Native',
+					avatar: 'https://placeimg.com/140/140/any',
+				},
+			},
+		]);
+
 		// Set the user's name as the screen title
 		navigation.setOptions({ title: name });
 	}, [name, navigation]);
 
 	return (
-		<View style={[styles.container, { backgroundColor: color }]}>
-			<Text style={styles.text}>Welcome to the chat, {name}!</Text>
+		<View style={[styles.chatContainer, { backgroundColor: color }]}>
+			{/* Render the GiftedChat component */}
+			<GiftedChat
+				messages={messages}
+				onSend={(newMessages) => onSend(newMessages)}
+				user={{
+					_id: 1,
+				}}
+			/>
+			{Platform.OS === 'android' ? (
+				<KeyboardAvoidingView behavior="height" />
+			) : null}
 		</View>
 	);
 };
@@ -21,6 +59,9 @@ const styles = StyleSheet.create({
 		flex: 1,
 		alignItems: 'center',
 		justifyContent: 'center',
+	},
+	chatContainer: {
+		flex: 1,
 	},
 	text: {
 		fontSize: 16,
